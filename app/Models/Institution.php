b95e3811f9e9bd2ee8a753036c75bd4a0874ddb1
Thiago Pereira <thiago.pereira7ps@gmail.com>
2024-09-19 14:17:32 -0300

    fix: Adjust institutions to use softdelete

--- app/Models/Institution.php
+++ app/Models/Institution.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Institution extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     /**
      * The attributes that are mass assignable.
      *
